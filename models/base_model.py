8c7d06a930aea1f46fb89351b978633e86e87039
Loay Al-Said <l.programming555@gmail.com>
2024-04-03 15:16:09 +0200

    Fix the creattoni of objects from code

--- models/base_model.py
+++ models/base_model.py
@@ -23,9 +23,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = kwargs['created_at'] = datetime.strptime(
-                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if not kwargs.get('id'):
+                kwargs['id'] = str(uuid.uuid4())
+            if kwargs.get('created_at'):
+                kwargs['updated_at'] = kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs['created_at'] = kwargs['updated_at'] = datetime.now()
+            if kwargs.get("__class__"):
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
