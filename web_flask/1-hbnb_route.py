7de929edb92816652902d05d0068db83d0cc682f
Loay Al-Said <l.programming555@gmail.com>
2024-04-20 22:32:53 +0200

    web_flask/1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create a Flask web application
+    initially, serving the route/
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def Greet():
+    """Return text: Hello HBNB!"""
+    return escape("Hello HBNB!")
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Return text: HBNB"""
+    return escape("HBNB")
+
+
+if __name__ == "__main__":
+    app.run()
