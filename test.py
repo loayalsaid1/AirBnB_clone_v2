1b7f092d0571ce0910fee007874b12cfd11438d8
Loay Al-Said <l.programming555@gmail.com>
2024-04-25 22:07:44 +0200

    web_flask/7-states_list.py

--- test.py
+++ test.py
@@ -1,35 +1,35 @@
-from models import *
+#!/usr/bin/python3
+"""
+Fill the database with states and cities
+"""
+from models import storage
 from models.state import State
 from models.city import City
-from models.user import User
-from models.place import Place
-from models.amenity import Amenity
+import uuid
 
-s = State(name="California")
-s.save()
-c = City(state_id=s.id, name="San Francisco")
-c.save()
+# Create a dictionary to hold state names and corresponding city names
+states_cities = {
+    "Alabama": ["Akron", "Babbie", "Calera", "Fairfield"],
+    "Arizona": ["Douglas", "Kearny", "Tempe"],
+    "California": ["Fremont", "Napa", "San Francisco", "San Jose", "Sonoma"],
+    "Colorado": ["Denver"],
+    "Florida": ["Miami", "Orlando"],
+    "Georgia": [],
+    "Hawaii": ["Honolulu", "Kailua", "Pearl city"],
+    "Illinois": ["Chicago", "Joliet", "Naperville", "Peoria", "Urbana"],
+    "Indiana": [],
+    "Louisiana": ["Baton rouge", "Lafayette", "New Orleans"],
+    "Minnesota": ["Saint Paul"],
+    "Mississippi": ["Jackson", "Meridian", "Tupelo"],
+    "Oregon": ["Eugene", "Portland"]
+}
 
-u = User(email="a@a.com", password="pwd")
-u.save()
+for s, c in states_cities.items():
+    state = State(name=s)
+    state.save()
+    for cc in c:
+        city = City(name=cc, state_id=state.id)
+        city.save()
 
-p1 = Place(user_id=u.id, city_id=c.id, name="House 1")
-p1.save()
-p2 = Place(user_id=u.id, city_id=c.id, name="House 2")
-p2.save()
 
-a1 = Amenity(name="Wifi")
-a1.save()
-a2 = Amenity(name="Cable")
-a2.save()
-a3 = Amenity(name="Eth")
-a3.save()
 
-p1.amenities.append(a1)
-p1.amenities.append(a2)
-
-p2.amenities.append(a1)
-p2.amenities.append(a2)
-p2.amenities.append(a3)
-
-storage.save()
