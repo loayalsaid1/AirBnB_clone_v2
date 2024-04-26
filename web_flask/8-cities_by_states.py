5f78c57d5b7be2bcecbfc80147957b816281034b
Loay Al-Said <l.programming555@gmail.com>
2024-04-27 01:09:41 +0300

    web_flask/8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """Route to show all the states in the database"""
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
